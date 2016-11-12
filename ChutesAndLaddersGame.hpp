b55e2632a0a1bc977a08edce4b98562445964bb1
gusq21 <gusq21@csu.fullerton.edu>
2016-11-11 23:51:00 -0800

    Update ChutesAndLaddersGame.hpp

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -8,44 +8,53 @@
 #include <string>
 
 #include "ArrayQueue.h"
-#include "Player.hpp"
-#include "GameBoard.hpp"
+#include "Player.h"
+#include "GameBoard.h"
 
 using namespace std;
+ArrayQueue<int> playerQueue;
 
 // TO DO - change the default value to the number of your team members
-const int MIN_NUMBER_OF_PLAYERS = 2;
+const int MIN_NUMBER_OF_PLAYERS = 1;
 
 class ChutesAndLaddersGame {
 public:
-   // TODO: implement the constructor with all your team members
-   // constructor with the default value of 2 minimim players
-   ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS);
+	// TODO: implement the constructor with all your team members
+	// constructor with the default value of 2 minimim players
+	ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS) {
+		
+		playerQueue.enqueue;
 
-   // TODO: implement the destructor
-   // destructor
-   ~ChutesAndLaddersGame();
-   
-   // accessors
-   // TO DO: implement the accessor to get the number Of Players
-   long getNumberOfPlayers() {
-      // TODO: implement this function properly
-      throw std::logic_error("not implemented yet");
-   }
-   string getWinner() { return winner; }
-   
-   // TODO: implement this function properly
-   // reset the game - (1) this will reset the position of each player to zero
-   void resetGame();
-   
-   // TODO: implement this function properly
-   // play the Chutes and Ladders game
-   void playGame();
+		
+
+	}
+
+	// TODO: implement the destructor
+	// destructor
+	~ChutesAndLaddersGame() {
+		playerQueue.dequeue;
+	}
+
+	// accessors
+	// TO DO: implement the accessor to get the number Of Players
+	long getNumberOfPlayers() {
+		// TODO: implement this function properly
+		throw std::logic_error("not implemented yet");
+	}
+	string getWinner() { return winner; }
+
+	// TODO: implement this function properly
+	// reset the game - (1) this will reset the position of each player to zero
+	void resetGame();
+
+	// TODO: implement this function properly
+	// play the Chutes and Ladders game
+	void playGame();
 private:
-   const int WINNING_POSITION = 100;
-   
-   string winner;       // the winner
-   GameBoard gameBoard; // the game board
+	const int WINNING_POSITION = 100;
 
-   // TO DO: use ArrayQueue to store the players
+	string winner;       // the winner
+	GameBoard gameBoard; // the game board
+
+						 // TO DO: use ArrayQueue to store the players
 };
